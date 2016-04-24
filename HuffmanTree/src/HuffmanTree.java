9f9514aef28a041eafd53c9397f397ccade7940f
SecondThread <13dharmeyer@gmail.com>
2016-04-24 04:47:20 +0000

    added node files

---
+++ HuffmanTree/src/HuffmanTree.java
@@ -0,0 +1,5 @@
+public class HuffmanTree {
+	public static void main(String[] a) {
+		System.out.println("asdf");
+	}
+}
---
+++ HuffmanTree/src/Node.java
@@ -0,0 +1,16 @@
+public class Node {
+	public Node left, right
+	public double value;
+	public String name;
+
+	public Node(Node left, Node right) {
+		this.left=left;
+		this.right=right;
+		value=left.value+right.value;
+	}
+
+	public Node(String name, double frequency) {
+		this.name=name;
+		value=frequency;
+	}
+}
